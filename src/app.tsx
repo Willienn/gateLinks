29d5d70d8f3ada59d39a344c2fd1e15d97ed3056
Willien <willien_dev@outlook.com>
2023-08-04 14:03:51 -0300

    the really initial commit

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,17 @@
-import './app.css'
+import "./app.css";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 export function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
-
-    </>
-  )
+    <Box>
+      <Heading>Test</Heading>
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+        beatae c ulpa debitis delectus deserunt dolor ea enim est facilis fugiat
+        harum incidunt, iste, laudantium, odio omnis quae quaerat quam vero?
+      </Text>
+    </Box>
+  );
 }
